59475e1a8da78e7af62a69286b96933470ac2249
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-18 19:24:08 -0300

    praparando para deploy

--- backendai/enviroments.py
+++ backendai/enviroments.py
@@ -1,15 +1,7 @@
-# COLLECTIONS_EMW = {
-#     "solar" : "emw_solar_radiation_irradiance",
-#     "pressure" : "emw_atm_pressure_pressure",
-#     "rain" : "emw_rain_level_length",
-#     "wind" : "emw_average_wind_speed_velocity",
-#     "gust" : "emw_gust_wind_speed_velocity",
-#     "dir" : "emw_wind_direction_angle",
-#     "temp" : "emw_temperature_temperature",
-#     "humid" : "emw_humidity_humidity",
-#     "lum" : "emw_luminosity_lux",
-#     "uv" : "emw_uv_dimensionless"
-# }
+# URI_MONGO = "mongodb://admin:123456@localhost:27017/admin"
+URI_MONGO = "mongodb://root:e3wsd4rpo98ijnG6T24443s1qSed@localhost:27017/admin"
+DB_NAME = 'projeto_agro_ai'
+LSTM_FORECAST_COLLECTION = 'lstm_forecast'
 
 COLLECTIONS_EMW = {
     "solar" : "emw_solar_radiation_irradiance",
