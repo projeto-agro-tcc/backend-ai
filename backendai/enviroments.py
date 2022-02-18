ad88a91e74d15e78d44beab5c2df24cb86ab8cfe
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-18 20:00:24 -0300

    praparando para deploy

--- backendai/enviroments.py
+++ backendai/enviroments.py
@@ -16,4 +16,5 @@ COLLECTIONS_EMW = {
     "uv" : "emw_uv_dimensionless"
 }
 
-API_IOT = "http://localhost:8002/iot/findbyparams"
+#API_IOT = "http://localhost:8002/iot/findbyparams"
+API_IOT = 'http://backend-iot-env.eba-prhv4rz9.sa-east-1.elasticbeanstalk.com/iot/findbyparams'
